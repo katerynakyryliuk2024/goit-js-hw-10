825bc33e7fbc5ba052fffa5872ffd459412b3746
katerynakyryliuk2024 <katerynaerdek@gmail.com>
2025-01-13 17:03:05 +0000

    Deploying to gh-pages from @ katerynakyryliuk2024/goit-js-hw-10@0cc6688672e3b324a837197b5d45778047ff7bd2 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import"./assets/vendor-BbbuE1sJ.js";const o=document.querySelector(".delay-input");console.dir(o);
+//# sourceMappingURL=2-snackbar.js.map
