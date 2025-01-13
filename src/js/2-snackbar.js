28744f146fb67c326dfa5d302874428ce64b7bfc
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-01-13 20:30:14 +0300

    Update 2-snackbar.js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -3,3 +3,16 @@ import "izitoast/dist/css/iziToast.min.css";
 
 const delayİnput = document.querySelector('.delay-input');
 console.dir(delayİnput);
+delayİnput.addEventListener('input', () => {
+    const inputValue = delayİnput.value.trim();
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject(`❌ Rejected promise in ${inputValue}ms`);
+            resolve(`✅ Fulfilled promise in ${inputValue}ms`);
+        }, inputValue);
+    })
+    Promise.resolve();
+        .then(value => console.log(value));
+
+})
