00f1efea910d0299aab7df6e3817ca649ca15548
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-01-15 17:09:11 +0300

    Update 2-snackbar.js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -12,7 +12,7 @@ formEl.addEventListener('submit', event => {
     const inputValue = event.target[0].value;
     console.dir(inputValue);
     
-    console.dir(event.target[2]);
+    console.dir(event.target[2].checked);
 
    new Promise((resolve, reject) => {
         setTimeout(() => {
